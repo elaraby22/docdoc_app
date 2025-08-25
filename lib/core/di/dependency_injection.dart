a83558ff9e3a7ec7f01578558cb634d2e6db83d7
elaraby22 <mohamedelaraby254@gmail.com>
2025-08-25 20:38:23 +0300

    Added sign up ui. Integrated with sign up EP. Added sign up repo and cubit. Added sign up states with validation.

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,3 +1,5 @@
+import 'package:curely_app/features/sign_up/data/repos/sign_up_repo.dart';
+import 'package:curely_app/features/sign_up/logic/sign_up_cubit.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -15,5 +17,9 @@ Future<void> setupGetIt() async {
 
   // login
   getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
-  getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
+
+  // sign up
+  getIt.registerLazySingleton<SignupRepo>(() => SignupRepo(getIt()));
+  getIt.registerFactory<SignupCubit>(() => SignupCubit(getIt()));
 }
--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -2,11 +2,13 @@ import 'package:curely_app/core/di/dependency_injection.dart';
 import 'package:curely_app/core/routing/routes.dart';
 import 'package:curely_app/features/home/ui/home_screen.dart';
 import 'package:curely_app/features/login/logic/login_cubit/login_cubit.dart';
+import 'package:curely_app/features/sign_up/logic/sign_up_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../features/login/ui/login_screen.dart';
 import '../../features/onboarding/onboarding_screen.dart';
+import '../../features/sign_up/ui/sign_up_screen.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
@@ -22,6 +24,11 @@ class AppRouter {
             builder: (_) => BlocProvider(
                 create: (context) => getIt<LoginCubit>(),
                 child: const LoginScreen()));
+      case Routes.signUpScreen:
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                create: (context) => getIt<SignupCubit>(),
+                child: const SignupScreen()));
       case Routes.homeScreen:
         return MaterialPageRoute(
           builder: (_) => const HomeScreen(),
